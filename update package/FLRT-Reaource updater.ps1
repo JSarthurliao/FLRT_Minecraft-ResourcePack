a62b32cc69f0f16d510db81b6a73ecee7bd50aca
JS_arthurliao <121561967+JSarthurliao@users.noreply.github.com>
2024-11-11 17:36:18 +0800

    Update "FLRT-Reaource updater.ps1"

--- update package/FLRT-Reaource updater.ps1
+++ update package/FLRT-Reaource updater.ps1
@@ -16,3 +16,12 @@ $SetUp = {
 		New-Item -Path "$path" -Name "temp" -ItemType "directory"
 		&$Update
 }
+
+$carlang_zncn = 
+
+
+$Update = {
+	Write-Output "downloading [car]zn_cn.json..."
+	Invoke-WebRequest -Uri $carlang_zncn -OutFile "$path\temp\car\"
+	Invoke-WebRequest -Uri $
+}
