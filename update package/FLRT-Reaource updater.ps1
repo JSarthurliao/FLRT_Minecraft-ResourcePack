e78c4d52a1b2951979248cbd0963c2f396cf8fd6
JS_arthurliao <121561967+JSarthurliao@users.noreply.github.com>
2024-11-11 16:08:30 +0800

    Update "FLRT-Reaource updater.ps1"

--- update package/FLRT-Reaource updater.ps1
+++ update package/FLRT-Reaource updater.ps1
@@ -2,9 +2,17 @@ $AskPath = {
 	$path = Read-Host "Please enter the path to FLRT-Resource vA1.8.zip:"
 		if (Test-Path -Path $path) {
 			"$path existed, moving to update!"
+			&$Update
 	} else {
 			"$path does not existed, please retry it agin!"
 			&$AskPath
 	}
 }
 
+$SetUp = {
+	if (Test-Path -Path $path\temp) {
+		&$Update
+} else {
+		New-Item -Path "$path" -Name "temp" -ItemType "directory"
+		&$Update
+}
