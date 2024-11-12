186e737613a994b6eff73424c4d2ffed39e3e61f
JS_arthurliao <121561967+JSarthurliao@users.noreply.github.com>
2024-11-12 14:48:56 +0800

    Update "FLRT-Reaource updater.ps1"

--- update package/FLRT-Reaource updater.ps1
+++ update package/FLRT-Reaource updater.ps1
@@ -18,10 +18,15 @@ $SetUp = {
 }
 
 $carlang_zncn = 
-
+$eyemod_zncn = 
 
 $Update = {
 	Write-Output "downloading [car]zn_cn.json..."
-	Invoke-WebRequest -Uri $carlang_zncn -OutFile "$path\temp\car\"
-	Invoke-WebRequest -Uri $
+	Invoke-WebRequest -Uri $carlang_zncn -OutFile "$path\temp\car\lang\zn_cn.json"
+ 	Write-Output "Done!"
+  
+ 	Write-Output "downloading [eyemod]zn_cn.json..."
+	Invoke-WebRequest -Uri $eyemod_zncn -OutFile "$path\temp\eyemod\lang\zn_zn.json"
+ 	Write-Output "Done!"
+ 	
 }
